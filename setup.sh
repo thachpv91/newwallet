41ad932efecdc0665ae26e272d9a45ce0f50b23c
Phan Van Thach <thachphanvan@gmail.com>
2016-12-05 14:04:26 +0700

    fixed version error

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 #	<artifactId>digitalcoinj-core</artifactId>
 #	<version>0.12.3</version>
 
-FULL_PATH=$PWD/libs/digitalcoinj-core-0.12.3.jar
+FULL_PATH=$PWD/libs/bapcoinj-core-0.12.3.jar
 GROUP_ID=org.bitcoinj
-ARTIFACT_ID=digitalcoinj-core
+ARTIFACT_ID=bapcoinj-core
 VERSION=0.12.3
 PACKAGING=jar
 
-mvn install:install-file  -Dfile=$FULL_PATH -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=VERSION -Dpackaging=$PACKAGING
+mvn install:install-file  -Dfile=$FULL_PATH -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dpackaging=$PACKAGING
