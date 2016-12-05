c3ab64ae01af2a57e840df35ead0d1d2c2fbc63d
Phan Van Thach <thachphanvan@gmail.com>
2016-12-05 10:09:34 +0700

    setup lib digitalcoinj-0.12.3

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
-export FULL_PATH=/home/bapvn/Downloads/ZIPFiles/core-1.54.0.0.jar
-export GROUP_ID=com.madgag.spongycastle
-export ARTIFACT_ID=core
-export VERSION=1.54.0.0
 
-mvn install:install-file  -Dfile=$FULL_PATH -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=VERSION -Dpackaging=jar
+#	<groupId>org.bitcoinj</groupId>
+#	<artifactId>digitalcoinj-core</artifactId>
+#	<version>0.12.3</version>
+
+export FULL_PATH=/home/bapvn/Downloads/digitalcoinj-core-0.12.3
+export GROUP_ID=org.bitcoinj
+export ARTIFACT_ID=digitalcoinj-core
+export VERSION=0.12.3
+export PACKAGING=jar
+
+mvn install:install-file  -Dfile=$FULL_PATH -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=VERSION -Dpackaging=$PACKAGING
